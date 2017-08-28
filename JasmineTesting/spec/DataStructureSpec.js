ea1aa5f10905669081ae48939e8ca1a1aac6712a
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-08-28 11:41:42 -0700

    add starter code and testing for queue

--- JasmineTesting/spec/DataStructureSpec.js
+++ JasmineTesting/spec/DataStructureSpec.js
@@ -64,13 +64,13 @@ describe("The Stack data structure", function() {
     stack = new Stack();
   });
 
-  it("should be able to push new values", function() {
+  it("should push new values", function() {
     stack.push("abc");
     stack.push(1234);
     expect(stack.storage).toEqual(["abc", 1234]);
   });
 
-  describe("when you call pop on it,", function() {
+  describe("when you call pop,", function() {
     var popped;
     it("should remove the last value", function() {
       popped = stack.pop();
@@ -82,7 +82,29 @@ describe("The Stack data structure", function() {
 
   })
 
-  it("should be able tell you the size", function() {
+  it("should tell you the size", function() {
     expect(stack.storage.size).toEqual(1);
   });
 });
+
+describe("The Queue data structure", function() {
+  var queue;
+
+  beforeEach(function() {
+    queue = new Queue;
+  });
+
+  it("should enqueue new values", function() {
+    queue.enqueue(75);
+    queue.enqueue(94);
+    queue.enqueue(3);
+    expect(queue.storage).toEqual({0: 75, 1: 94, 2: 3})
+  });
+  it("should dequeue", function() {
+    queue.dequeue();
+    expect(queue.head).toEqual(94);
+  });
+  it("should tell you the size", function() {
+    expect(queue.size).toEqual(2);
+  })
+})
---
+++ JasmineTesting/src/Queue.js
@@ -0,0 +1,18 @@
+class Queue {
+  constructor() {
+    //initiate the storage to be an empty object and keep track of the head and tail
+
+  }
+
+  enqueue(val) {
+
+  }
+
+  dequeue() {
+
+  }
+
+  size() {
+
+  }
+}
